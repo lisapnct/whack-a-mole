8197aa0eeb47bf42f88245991d96f404f7ff3228
Lisa Poncet <poncet.lisa@gmail.com>
2020-09-07 18:10:20 +0200

    added some sprite animations and js logic

--- MVP.md
+++ MVP.md
@@ -15,10 +15,12 @@ Rules:
 
 Javascript logic todo:
 
-- an array of moles?
 - display characters (moles, holes) on a random grid square for a given time (ex: 2sec)
 - reduce moles display time gradually && increase nb of moles displayed gradually?
 - on click on a mole => counter += Xpts
 - one minute timer:
     - setInterval 1000ms => currentTime += 1
     - when currentTime >= 60 => end game
+
+
+
--- app.js
+++ app.js
@@ -1,55 +1,97 @@
-const square = document.querySelectorAll(".square");
+// const square = document.querySelectorAll(".square");
+const hole = document.querySelectorAll(".hole");
 const mole = document.querySelectorAll(".mole");
-const timeLeft = document.querySelector("#time-left");
+// const rabbit = document.querySelectorAll(".rabbit");
+const livesLeft = document.querySelector("#fas");
 const score = document.querySelector("#score");
-// console.log(allSquares);
-// console.log(score);
 
+let totalLives = 3;
 let finalScore = 0;
-let currentTime = timeLeft.innerText;
-let winSquareId = "";
+// let currentTime = 30;
 
-function randomSquare() {
-  square.forEach((square) => {
-    square.classList.remove("mole");
-  });
-
-  let randomPosition = square[Math.floor(Math.random() * 9)];
-  randomPosition.classList.add("mole");
-
-  winSquareId = randomPosition.id;
-}
-
-function moveMole() {
-  let molesMoving = setInterval(randomSquare, 900);
-  if (currentTime === 0) {
-    clearInterval(molesMoving);
+function displayMole() {
+  let randomPosition = hole[Math.floor(Math.random() * 12)];
+  // check if randomPosition already has bob class
+  if (randomPosition.classList.contains('bob-out')) {
+    displayMole();
+  } else {
+    randomPosition.classList.add("mole-out");
   }
 }
 
-// moveMole();
+function hideMole() {
+  hole.forEach((hole) => {
+    hole.classList.remove("mole-out");
+  });
+}
 
-let timer = setInterval(countDown, 1000);
+function moveMole() {
+  setInterval(displayMole, 1200);
+  setInterval(hideMole, 2400);
+}
 
-function countDown() {
-  currentTime--;
-  timeLeft.textContent = currentTime;
-  console.log(currentTime);
+function displayBob() {
+  let randomPosition = hole[Math.floor(Math.random() * 12)];
+  if (randomPosition.classList.contains('mole-out')) {
+    displayBob();
+  } else {
+    randomPosition.classList.add("bob-out");
+  }
+  // to avoid displaying rabbit on mole: if randomPosition !contains "mole" class = display else displayRabbit() (= loop?)
+}
 
-  if (currentTime === 0) {
-    clearInterval(timer);
+function hideBob() {
+  hole.forEach((hole) => {
+    hole.classList.remove("bob-out");
+    hole.classList.remove("mole-hit");
+  });
+}
+
+function moveBob() {
+  setInterval(displayBob, 3000);
+  setInterval(hideBob, 6000);
+}
+
+function countLives() {
+  totalLives = totalLives - 1;
+
+  if (totalLives === 0) {
     alert("GAME OVER!! Your final score is: " + finalScore + "points");
   }
 }
 
-square.forEach((square) => {
-  square.addEventListener("click", () => {
-    if (square.id === winSquareId) {
-      finalScore = finalScore + 1;
-      score.innerText = finalScore;
-    }
+// calculate score depending on which square the user clicked (based on classList)
+function calculateScore(clickedHole) {
+  // on click on a mole : +1 point
+  if (clickedHole.classList.contains("mole-out")) {
+    console.log("it's a mole");
+    clickedHole.classList.add("mole-hit");
+    finalScore = finalScore + 1;
+    score.innerText = finalScore;
+  }
+
+  //on click on a rabbit: -2 points
+  if (clickedHole.classList.contains("bob-out")) {
+    console.log("it's bob!!");
+    finalScore = finalScore - 1;
+    countLives();
+  }
+}
+
+// listen to all clicks and run calculateScore on the event.target
+hole.forEach((hole) => {
+  hole.addEventListener("click", (evt) => {
+    let clickedHole = evt.target;
+    calculateScore(clickedHole);
   });
 });
 
-// TODO
-// - alert: start game => lance compteur et moveMole()
+window.addEventListener("load", () => {
+  alert("START GAME");
+  moveMole();
+  moveBob();
+});
+
+// -------------
+// - score should not be < 0
+// - add a if(...) on set mole/rabbit class to avoid square with mole + rabbit classes
